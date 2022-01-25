76ae95d500661ccb2c6aceb7ab3d98ff70e64c18
Lucas Rocha <lucasdejesus.rocha@hotmail.com>
2022-01-25 19:41:29 -0300

    Tô tentando

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // seu código aqui
+  const searchSpecies = data.species.filter((obj) => obj.id === ids);
+  // console.log(searchSpecies);
+  return searchSpecies;
 }
 
 module.exports = getSpeciesByIds;
