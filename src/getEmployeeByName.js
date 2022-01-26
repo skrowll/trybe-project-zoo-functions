713862fd996de8f8f93ce37b09ddeda2caf1c050
Lucas Rocha <lucasdejesus.rocha@hotmail.com>
2022-01-26 18:50:46 -0300

    Requisito 3 OK

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,6 +2,11 @@ const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  if (employeeName === undefined) {
+    return {};
+  }
+  const searchEmployees = (data.employees.find(({ firstName }) => employeeName === firstName)) || (data.employees.find(({ lastName }) => employeeName === lastName));
+  return searchEmployees;
 }
 
 module.exports = getEmployeeByName;
