18570f12837ceffe9e399ba10cc3db6ae7bef1fd
Lucas Rocha <lucasdejesus.rocha@hotmail.com>
2022-01-30 03:36:21 -0300

    Requisito 6 OK

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -2,10 +2,25 @@ const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
   // seu código aqui
+  if (!entrants || entrants.length === undefined) return 0;
+  const entrantsAmount = {};
+  const childAmount = entrants.filter((item) => item.age < 18).length;
+  const adultAmount = entrants.filter((item) => item.age >= 18 && item.age < 50).length;
+  const seniorAmount = entrants.filter((item) => item.age >= 50).length;
+  entrantsAmount.child = childAmount;
+  entrantsAmount.adult = adultAmount;
+  entrantsAmount.senior = seniorAmount;
+  return entrantsAmount;
 }
 
 function calculateEntry(entrants) {
   // seu código aqui
+  if (!entrants || entrants.length === undefined) return 0;
+  const priceChild = entrants.filter((item) => item.age < 18).length * data.prices.child;
+  const adultAmount = entrants.filter((item) => item.age >= 18 && item.age < 50).length;
+  const priceAdult = adultAmount * data.prices.adult;
+  const priceSenior = entrants.filter((item) => item.age >= 50).length * data.prices.senior;
+  return priceChild + priceAdult + priceSenior;
 }
 
 module.exports = { calculateEntry, countEntrants };
