d3bb81d44f2e8a2908241e7dcd4544a0cb739cc8
Lucas Rocha <lucasdejesus.rocha@hotmail.com>
2022-01-30 01:41:02 -0300

    Requisito 5 OK

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -2,6 +2,21 @@ const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
   // seu código aqui
+  if (!animal) {
+    const animalList = {};
+    data.species.forEach((item) => {
+      animalList[item.name] = item.residents.length;
+    });
+    return animalList;
+  }
+  if (!animal.sex) {
+    const findAnimal = data.species.find((item) => item.name === animal.specie);
+    const specieAmount = findAnimal.residents.length;
+    return specieAmount;
+  }
+  const findAnimal = data.species.find((item) => item.name === animal.specie);
+  const compareSex = findAnimal.residents.filter((item) => item.sex === animal.sex);
+  return compareSex.length;
 }
 
 module.exports = countAnimals;
