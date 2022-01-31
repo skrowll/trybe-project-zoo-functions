0ab0864172162b8b009327fcc10af6752db756d5
Lucas Rocha <lucasdejesus.rocha@hotmail.com>
2022-01-30 21:15:05 -0300

    Inicio Requisito 7 e teste avaliador

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -2,6 +2,20 @@ const data = require('../data/zoo_data');
 
 function getAnimalMap(options) {
   // seu código aqui
+  if (!options) {
+    const animalsLocation = {};
+    const locations = data.species.map((item) => item.location);
+    // https://pt.stackoverflow.com/questions/16483/remover-elementos-repetido-dentro-de-um-array-em-javascript
+    const filterLocation = locations.filter((item, index) => locations.indexOf(item) === index);
+    // const x = data.species.filter((item) => item.location === filterLocation[0]);
+
+    });
+    console.log(filterLocation);
+    console.log(animalsLocation);
+    console.log(x.map((item) => item.name));
+    console.log(y);
+  }
 }
 
+getAnimalMap();
 module.exports = getAnimalMap;
