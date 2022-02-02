da1e07c017979874355a99c3af576cc9535fa1d4
Lucas Rocha <lucasdejesus.rocha@hotmail.com>
2022-02-02 00:07:12 -0300

    Requisito 8 OK - Agora vou dormir zzz

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,26 @@
 const data = require('../data/zoo_data');
 
+const objectWeek = data.hours;
+const days = Object.keys(objectWeek);
+const completeSchedule = {};
+
 function getSchedule(scheduleTarget) {
   // seu código aqui
+  days.forEach((item) => {
+    completeSchedule[`${item}`] = (
+      { officeHour: `Open from ${objectWeek[item].open}am until ${objectWeek[item].close}pm`,
+        exhibition: data.species.filter((item2) => item2.availability.includes(item))
+          .map((item3) => item3.name) });
+  });
+  completeSchedule.Monday.officeHour = 'CLOSED';
+  completeSchedule.Monday.exhibition = 'The zoo will be closed!';
+  if (days.includes(scheduleTarget)) {
+    return { [`${scheduleTarget}`]: completeSchedule[scheduleTarget] };
+  }
+  if (data.species.map((item) => item.name).includes(scheduleTarget)) {
+    return data.species.find((item2) => item2.name === scheduleTarget).availability;
+  }
+  return completeSchedule;
 }
 
 module.exports = getSchedule;
